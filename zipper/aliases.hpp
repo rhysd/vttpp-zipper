de5dde54db78dbd16fe581f4dddfba2e9dab0319
rhysd <lin90162@yahoo.co.jp>
2013-09-13 17:03:10 +0900

    add some aliases

--- zipper/aliases.hpp
+++ zipper/aliases.hpp
@@ -1,10 +1,15 @@
 #if !defined ZIPPER_ALIASES_HPP_INCLUDED
 #define      ZIPPER_ALIASES_HPP_INCLUDED
 
+#include <cstddef>
+
 #include "./zipper.hpp"
+#include "./make_pack.hpp"
 
 namespace zipper {
 namespace alias {
+
+using std::size_t;
 
 template<class Z>
 using before = typename ::zipper::before<Z>::type;
@@ -18,8 +23,11 @@ using next = typename ::zipper::next<Z>::type;
 template<class Z>
 using prior = typename ::zipper::prior<Z>::type;
 
+template<class P>
+using zipper_of = typename ::zipper::zipper_of<P>::type;
+
 template<class... Elems>
-using zipper_of = typename ::zipper::zipper_of<Elems...>::type;
+using emplace_zipper = typename ::zipper::emplace_zipper<Elems...>::type;
 
 template<class Z, class T>
 using insert_before = typename ::zipper::insert_before<Z, T>::type;
@@ -35,6 +43,27 @@ using remove_after = typename ::zipper::remove_after<Z>::type;
 
 template<class Z>
 using pack_of = typename ::zipper::pack_of<Z>::type;
+
+template<class Z, size_t N>
+using advance = typename ::zipper::advance<Z, N>::type;
+
+template<class Z, size_t N>
+using retreat = typename ::zipper::retreat<Z, N>::type;
+
+template<class Z, class...XS>
+using add_all_before = typename ::zipper::add_all_before<Z, XS...>::type;
+
+template<class Z, class...XS>
+using add_all_after = typename ::zipper::add_all_after<Z, XS...>::type;
+
+template<class Z>
+using swap = typename swap<Z>::type;
+
+template<class... Args>
+using make_pack = typename make_pack<Args...>::type;
+
+template<class T>
+  using make_zipper = typename make_zipper<T>::type;
 
 } // namespace alias
 } // namespace zipper
